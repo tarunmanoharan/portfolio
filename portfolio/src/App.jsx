2035154b2af6b77b3c8ccd4a8af4d8fddea8c527
tarunmanoharan <98203435+tarunmanoharan@users.noreply.github.com>
2024-02-20 10:47:41 +0530

    beta

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./components/Home/Home";
 import {
   BrowserRouter as Router,
   Route,
@@ -10,23 +11,13 @@ import {
 import "./App.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div className="App">
         <Navbar />
         <Routes>
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="/" element={<Home/>} />
         </Routes>
         <Footer />
       </div>
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    
+   <Navbar>
+    <div className='navbar-items'>
+        <Link to ="/">Home</Link>
+    </div>
+   </Navbar>
+
+  )
+}
